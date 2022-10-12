8a253a9772e7cfaed61e1c0d60e5ecd1c4965ef4
railiavaliullina <railia.valiullina@gmail.com>
2022-10-12 09:53:17 +0300

    add ready static dashboard

--- dash_example.py
+++ dash_example.py
@@ -1,8 +1,117 @@
-from dash import Dash, dcc
+import dash
+import dash_bootstrap_components as dbc
+import dash_daq as daq
+import plotly.express as px
+from dash import dcc
+from dash import html
+from dash_bootstrap_templates import load_figure_template
 
-app = Dash(__name__)
+from dataset import Dataset
 
-app.layout = dcc.RadioItems(['New York City', 'Montreal', 'San Francisco'], 'Montreal', labelStyle={'display': 'block'})
+load_figure_template('LUX')
 
-if __name__ == "__main__":
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "24rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+sidebar = html.Div(
+    [
+        html.H1("Filters"),
+        html.Hr(),
+        dbc.Nav(
+            [
+                html.P(
+                    "View Images Samples", className="lead"
+                ),
+                html.Div([
+                    dcc.RadioItems([' with any boxes', ' without boxes'], ' with any boxes', inline=False,
+                                   labelStyle={'display': 'block'})], style={'marginLeft': '4%'}),
+                html.Br(),
+                html.Div([
+                    daq.BooleanSwitch(id='show-boxes-switch', on=True, label='Draw Boxes',
+                                      labelPosition='Right', color=px.colors.qualitative.G10[3]),
+                ], style={'marginLeft': '2%', 'width': '45%', 'marginBottom': '2%'}),
+                html.Br(),
+                html.Div(
+                    [
+                        dbc.Button("Prev", outline=True, color="secondary", className="me-1"),
+                        dbc.Button(
+                            "Next", outline=True, color="secondary", className="me-1"
+                        )], style={'marginBottom': '7%', 'marginLeft': '4%'}),
+                html.Hr(),
+                html.P(
+                    "View Training Results", className="lead"
+                ),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+app = dash.Dash(external_stylesheets=[dbc.themes.LUX])
+
+dataset = Dataset()
+
+img_fig_0 = dataset.figures[0]
+img_fig_1 = dataset.figures[0]
+
+# Hide the axes and the tooltips
+img_fig_0.update_layout(plot_bgcolor='white', paper_bgcolor='white', margin=dict(t=20, b=0, l=0, r=0),
+                        xaxis=dict(showgrid=False, showticklabels=False, linewidth=0),
+                        yaxis=dict(showgrid=False, showticklabels=False, linewidth=0),
+                        hovermode=False
+                        )
+img_fig_1.update_layout(plot_bgcolor='white', paper_bgcolor='white', margin=dict(t=20, b=0, l=0, r=0),
+                        xaxis=dict(showgrid=False, showticklabels=False, linewidth=0),
+                        yaxis=dict(showgrid=False, showticklabels=False, linewidth=0),
+                        hovermode=False
+                        )
+
+app.layout = html.Div(children=[
+
+    dbc.Row([
+        dbc.Col(),
+        dbc.Col(html.H3('Statistics'), width=9, style={'marginTop': '2%'}),
+    ]),
+    dbc.Row(
+        [dbc.Col(),
+         dbc.Col(html.Div("Images with and without labels"), style={'marginLeft': '4.5%', 'width': '40%'}),
+         dbc.Col(html.Div("Types of labels"), style={'marginLeft': '4.5%', 'width': '40%'}),
+         ],
+        className="g-0", style={'marginTop': '2%'}
+    ),
+    dbc.Row(
+        [
+            dbc.Col(dcc.Graph(id='graph1', figure=px.pie(dataset.labeled_and_non_labeled_data_num, values='num_images',
+                                                         names='labeled_or_not', hole=.3)), width=9,
+                    style={'width': '40%', 'marginLeft': '7%', 'display': 'inline-block'}),
+
+            dbc.Col(dcc.Graph(id='graph2', figure=px.pie(dataset.objects_id_counter, names='class',
+                                                         values='boxes_num', hole=.0)), width=9,
+                    style={'width': '40%', 'display': 'inline-block'})
+        ], style={'marginLeft': '20%'}),
+
+    dbc.Row([
+        dbc.Col(),
+        dbc.Col(html.H3('Dataset Viewer'), width=9, style={'marginTop': '1.5%'}),
+    ]),
+    dbc.Row(
+        [dbc.Col(sidebar, style={'display': 'inline-block'}),
+         dbc.Col(dcc.Graph(id=f'image-{0}', figure=img_fig_0),
+                 style={'display': 'inline-block'}),
+         dbc.Col(dcc.Graph(id=f'image-{1}', figure=img_fig_1),
+                 style={'display': 'inline-block', 'marginRight': '10%'})
+         ]),
+
+]
+)
+if __name__ == '__main__':
     app.run_server(debug=True)
--- dataset.py
+++ dataset.py
@@ -1,5 +1,8 @@
+import json
 import os
+from collections import Counter
 
+import dash_bootstrap_components as dbc
 import dash_daq as daq
 import numpy as np
 import pandas as pd
@@ -12,13 +15,18 @@ from config import args
 
 class Dataset:
     def __init__(self):
+
+        with open('dataset_config.json') as json_file:
+            self.dataset_cfg = json.load(json_file)
+
         self.dataset = self.get_dataset_as_dataframe()
         self.get_eda()
         print(args.data_path)
 
     @staticmethod
     def get_dashboard(figures):
-        app = Dash(__name__)  # TODO: prev, next; Boxes (turn_on); show labelled, unlabelled data;
+        app = Dash(external_stylesheets=[dbc.themes.LUX])
+        # TODO: prev, next; Boxes (turn_on); show labelled, unlabelled data;
 
         # graphs_list = [dcc.Graph(id=f'image-{fig_id}', figure=fig) for fig_id, fig in enumerate(figures)]
 
@@ -37,8 +45,9 @@ class Dataset:
                                   labelPosition='Right', color=px.colors.qualitative.Prism[4]),
             ], style={'margin': '2%', 'marginTop': '3%', 'width': '35%'})
         ],
-            style={'background-color': px.colors.qualitative.Prism[10], 'fontSize': 18, 'font-family': 'monospace',
-                   'width': '20%', 'margin': '2%', 'padding': '0.5%', 'color': 'white'})
+            style={
+                # 'background-color': px.colors.qualitative.Prism[10], 'fontSize': 18, 'font-family': 'monospace', 'color': 'white'
+                'width': '20%', 'margin': '2%', 'padding': '0.5%'})
         # px.colors.qualitative.G10[9]
         app.run_server()
 
@@ -55,11 +64,11 @@ class Dataset:
         isnan_labels_num = self.dataset.label.isna().sum()
 
         # visualize images sample
-        labeled_data_sample = self.dataset[~self.dataset.label.isna()].head(3)
-        non_labeled_data_sample = self.dataset[self.dataset.label.isna()].head(3)
+        labeled_data_sample = self.dataset[~self.dataset.label.isna()]
+        non_labeled_data_sample = self.dataset[self.dataset.label.isna()]
 
         figures = []
-        for row in labeled_data_sample.iterrows():
+        for row in labeled_data_sample.head(10).iterrows():
             image = Image.open(os.path.join(args.data_path, row[1]['img_path']))
             image_w, image_h = image.size
             fig = px.imshow(image)
@@ -76,11 +85,28 @@ class Dataset:
                               line=dict(color=px.colors.qualitative.Plotly[object_class], width=2),
                               )
                 fig.update_shapes(dict(xref='x', yref='y'))
-            # fig.show()
+                # fig.show()
+                # a = 1
             figures.append(fig)
 
-        self.get_dashboard(figures)
+        # self.get_dashboard(figures)
+        self.figures = figures
+        self.labeled_and_non_labeled_data_num = pd.DataFrame()
+        self.labeled_and_non_labeled_data_num['labeled_or_not'] = ['With',
+                                                                   'Without']
+        self.labeled_and_non_labeled_data_num['num_images'] = [len(labeled_data_sample),
+                                                               len(non_labeled_data_sample)]
+        objects_id_counter = Counter(
+            np.concatenate(self.dataset.label[~self.dataset.label.isna()].to_list())[:, 0].astype(int))
+        objects_id_dict = {
+            f'{self.dataset_cfg["class_id_2_class_name_mapping"][str(object_id)]}': boxes_num for
+            object_id, boxes_num in objects_id_counter.items()}
 
+        # self.objects_id_counter = objects_id_dict
+
+        self.objects_id_counter = pd.DataFrame()
+        self.objects_id_counter['class'] = list(objects_id_dict.keys())
+        self.objects_id_counter['boxes_num'] = list(objects_id_dict.values())
         pass
 
     def get_dataset_as_dataframe(self):
@@ -99,13 +125,22 @@ class Dataset:
         dataset_df['label'] = labels
         return dataset_df
 
-    @staticmethod
-    def read_labels(txt_files):
+    def read_labels(self, txt_files):
         labels = []
+        # self.object_ids = []
         for txt_file in txt_files:
             with open(os.path.join(args.data_path, txt_file), 'r') as f:
                 text = f.readlines()
             if text:
+                # boxes = []
+                # for line in text:
+                #     for num_id, num in enumerate(line.replace('\n', '').split(' ')):
+                #         if num_id == 0:
+                #             object_id = int(num)
+                #             boxes.append(object_id)
+                #             self.object_ids.append(object_id)
+                #         else:
+                #             boxes.append(float(num))
                 boxes = [[int(num) if num_id == 0 else float(num) for num_id, num in
                           enumerate(line.replace('\n', '').split(' '))] for line in text]
             else:
